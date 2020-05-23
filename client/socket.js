8d3ed9f98e376c0e094a258fbfdd191eca9d160b
ejmejm <ninjaedan@aol.com>
2020-05-23 01:09:54 -0700

    for florian

--- client/socket.js
+++ client/socket.js
@@ -3,23 +3,23 @@ import {writable} from 'svelte/store';
 
 export const playerCount = writable(0);
 
-export const socket = io.connect('http://animalese-chatroom.herokuapp.com:80');
+export const socket = io.connect('http://localhost:3000'); // 'http://animalese-chatroom.herokuapp.com:80');
 
 socket.on('connect', function () {
   console.log('[open] Connection established');
 });
 
 socket.on('playerMessage', function (data) {
-  const {text} = data;
+  const {text, name} = data;
   synthesizeText(text);
 });
 
 socket.on('playerConnected', function (data) {
-  const {socketId, nClients} = data;
+  const {socketId, nClients, name, file} = data;
   playerCount.set(nClients);
 });
 
 socket.on('playerDisconnected', function (data) {
-  const {socketId, nClients} = data;
+  const {socketId, nClients, name} = data;
   playerCount.set(nClients);
 });
